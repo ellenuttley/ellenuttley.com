eeccd0d82a7b3645aa0999df2c1638dc354bd675
ellenuttley <ellenuttley@gmail.com>
2024-03-19 01:39:58 +0000

    interim fix homepage on mobile

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,26 +3,26 @@ import portrait from '../images/portrait.png'
 
 const Homepage = () => (
     <div className="mygrid grid-cols-3 gap-1 bg-transparent border-none shadow-none">
-        <div className='flex flex-col justify-evenly h-[60%] text-center'>
-        <div className=" gridbox gridbox-info order-1 col-span-1 h-fit bg-mywhite border-primary shadow-darkPrimary shadow-sm text-center">
+        <div className='flex col-span-3 lg:col-span-1 flex-col justify-evenly h-[60%] text-center'>
+        <div className=" gridbox gridbox-info order-1 col-span-3 lg:col-span-1 w-fill h-fit bg-mywhite border-primary shadow-darkPrimary shadow-sm text-center">
             Hello, I am Ellen
         </div>
 
-        <div className="gridbox gridbox-info order-2 col-span-1 h-fit bg-mywhite border-primary shadow-darkPrimary shadow-sm text-center">
+        <div className="gridbox gridbox-info order-2 col-span-3 lg:col-span-1 w-fit h-fit bg-mywhite border-primary shadow-darkPrimary shadow-sm text-center">
             No, I don't really look like that
         </div>
 
-        <div className="gridbox gridbox-info order-2 col-span-1 h-fit bg-mywhite border-primary shadow-darkPrimary shadow-sm text-center">
+        <div className="gridbox gridbox-info order-2 col-span-3 lg:col-span-1 w-fit h-fit bg-mywhite border-primary shadow-darkPrimary shadow-sm text-center">
             ... I'm just good with AI 
         </div>
 
-        <div className=" gridbox gridbox-info order-3 col-span-1 h-fit bg-mywhite border-primary shadow-darkPrimary shadow-sm text-center">
+        <div className=" gridbox gridbox-info order-3 col-span-3 lg:col-span-1 w-fit h-fit bg-mywhite border-primary shadow-darkPrimary shadow-sm text-center">
         Press Some Buttons
         </div>
         </div>
 
-        <div className='gridbox col-span-2 order-1 border-none'>
-        <img className="h-[60%] w-auto rounded-lg p-6 bg-mywhite border-primary shadow-darkPrimary shadow-sm" src={portrait}  alt="Ellen Portrait" />
+        <div className='gridbox col-span-3 lg:col-span-2 order-1 border-none'>
+        <img className="w-fit h-auto rounded-lg p-6 bg-mywhite border-primary shadow-darkPrimary shadow-sm" src={portrait}  alt="Ellen Portrait" />
         </div>
     </div>
 );
