add96882278f67dcfe7e30a8eba5c79a8d3bffcc
ellenuttley <ellenuttley@gmail.com>
2024-04-26 15:10:11 +0100

    added blank design block document, for homepage

---
+++ src/components/DesignBlock.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const DesignTitle = () => {
+ return (
+    <h2>I Do Design</h2>
+ );
+};
+
+const DesignContent = () => {
+ return (
+    <div>
+      Design Block Content
+    </div>
+ );
+};
+
+export { DesignTitle, DesignContent };
