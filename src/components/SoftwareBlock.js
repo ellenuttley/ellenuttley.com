707d4779240666ccec84f0abe5ca8964c4b4f496
ellenuttley <ellenuttley@gmail.com>
2024-04-26 15:11:46 +0100

    added blank software block, for homepage

---
+++ src/components/SoftwareBlock.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SoftwareTitle = () => {
+ return (
+    <h2>I do software</h2>
+ );
+};
+
+const SoftwareContent = () => {
+ return (
+    <div>
+      Software Block Content
+    </div>
+ );
+};
+
+export { SoftwareTitle, SoftwareContent };
