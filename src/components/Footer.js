9955ac68c0ebff8f955e7780c521404481cb8a3a
ellenuttley <ellenuttley@gmail.com>
2024-04-29 12:20:47 +0100

    edited styling for darkmode. tooltip now visible

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
   };
 
   return (
-    <footer className="group fixed dark:bg-secondary  dark:text-lightPurple dark:shadow-lightPurple bottom-1 left-5 right-5 z-10 flex h-fit w-auto translate-y-[90%] transform flex-row justify-evenly rounded-md bg-mywhite pb-2 pt-2 font-lexend text-secondary shadow-sm shadow-secondary transition-transform duration-300 hover:translate-y-0  ;">
+    <footer className="group fixed dark:bg-secondary  dark:text-lightPurple dark:shadow-lightPurple dark:border-primary dark:border bottom-1 left-5 right-5 z-10 flex h-fit w-auto translate-y-[90%] transform flex-row justify-evenly rounded-md bg-mywhite pb-2 pt-2 font-lexend text-secondary shadow-sm shadow-secondary transition-transform duration-300 hover:translate-y-0  ;">
       <a
         href="https://github.com/ellenuttley/aboutme"
         target="_blank"
@@ -36,7 +36,7 @@ const Footer = () => {
         <FiClipboard size="22" className="mr-2" />
         <span className="">{email}</span>
         {showNotification && (
-          <div className="absolute translate-x-10 rounded bg-primary px-2 py-1 text-xs text-mywhite shadow-sm">
+          <div className="absolute  rounded bg-primary px-2 py-1 text-xs text-mywhite shadow-sm dark:bg-darkPrimary">
             Email copied to clipboard!
           </div>
         )}
