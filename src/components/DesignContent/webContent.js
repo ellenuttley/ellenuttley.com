1c3f30aabc1a582db04791b4139baddf2669e339
ellenuttley <ellenuttley@gmail.com>
2024-05-03 16:07:39 +0100

    added content to webcontent carousel on the design page

--- src/components/DesignContent/webContent.js
+++ src/components/DesignContent/webContent.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+// import webdesignClosed from "../../images/webdesignClosed.png";
+import webdesignOpen from "../../images/webdesignOpen.png";
 
 const WebContent = () => {
-    return (
-        <div>
-            {/* code */}
-        </div>
-    )
-}
+	return (
+		<div>
+			<div className="size-full">
+
+				<img
+					className="object-top object-fill size-full touch-manipulation  "
+					src={webdesignOpen}
+					alt="Detailed breakdown from a page of a mobile app"
+					loading="lazy"
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default WebContent;
