d6c53515199394ce575edb99eeb105069682c3f7
ellenuttley <ellenuttley@gmail.com>
2024-05-03 16:03:30 +0100

    added content to ux accordion

--- src/components/DesignContent/uxContent.js
+++ src/components/DesignContent/uxContent.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import foodApp from "../../images/foodApp.png";
+import petScreen from "../../images/petScreen.png";
+
 
 const UxContent = () => {
-    return (
-        <div>
-            {/* code */}
-        </div>
-    )
-}
+	return (
+		<div className="size-full">
+			<img
+				className="object-top object-fill size-full touch-manipulation  "
+				src={foodApp}
+				alt="Wireframes of a table ordering mobile app"
+				loading="lazy"
+			/>
+			<img
+				className="object-top object-fill size-full touch-manipulation  "
+				src={petScreen}
+				alt="Detailed breakdown from a page of a mobile app"
+				loading="lazy"
+			/>
+		</div>
+	);
+};
 
 export default UxContent;
