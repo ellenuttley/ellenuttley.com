1eb2d2cee2d12148b5ec4a0780ae811156783bc7
ellenuttley <ellenuttley@gmail.com>
2024-09-05 10:51:34 +0100

    Tab that I added to the menu, to improve user story on large screens

---
+++ src/components/MenuTab.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function MenuTab() {
+	return (
+		<div className="flex justify-center items-center ">
+			<div className=" bg-lightGrey  rounded-md p-1 shadow-md shadow-secondary text-center border-solid border-darkPrimary text-secondary">
+				M<br />E<br />N<br />U
+			</div>
+		</div>
+	)
+}
+
+export default MenuTab
