2ef29e6d6f6f2f38ff38c12ef2d8bc52fbce7b63
ellenuttley <ellenuttley@gmail.com>
2024-09-05 10:51:49 +0100

    Tab that I added to the footer, to improve user story on large screens

---
+++ src/components/FooterTab.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FooterTab() {
+  return (
+    <div className='flex justify-center items-center '>
+        <div className=' bg-lightGrey rounded-md px-2 text-base tracking-widest shadow-md shadow-secondary text-center border-solid border-darkPrimary text-secondary'>
+        LINKS
+        </div>
+      </div>
+  );
+}
+
+export default FooterTab;
