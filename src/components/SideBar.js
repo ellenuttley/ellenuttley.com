c201aab887b9ae48130b37cc910333b0a3c10b6d
ellenuttley <ellenuttley@gmail.com>
2024-09-05 10:49:33 +0100

    Linted sidebar code

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,26 +1,50 @@
-import { GiSunglasses, GiLightBulb, GiLaptop,  GiMushroomHouse, GiPalette} from 'react-icons/gi'
+import {
+	GiSunglasses,
+	GiLightBulb,
+	GiLaptop,
+	GiMushroomHouse,
+	GiPalette,
+} from "react-icons/gi"
+import MenuTab from "./MenuTab"
 // GiSpellBook,
 const SideBar = ({ setView, darkMode, toggleDarkMode }) => {
-
-    return (
-        <div className={`sidebar ${darkMode ? 'dark' : ''}`}>
-            <SideBarIcon icon={<GiMushroomHouse size="44"/>} text = 'Homepage' handleClick={setView}/>
-            <SideBarIcon icon={<GiSunglasses size="44"/>} text = 'Read My CV' handleClick={setView}/>
-            <SideBarIcon icon={<GiLaptop size="44"/>} text = 'My Projects' handleClick={setView}/>
-            <SideBarIcon icon={<GiPalette size="44"/>} text = 'My React Components' handleClick={setView}/>
-            <SideBarIcon icon={<GiLightBulb size="44"/>} text = 'Toggle Dark Mode' handleClick={toggleDarkMode}/>
-        </div>
-    );
-
-};
+	return (
+		<div className={`sidebar ${darkMode ? "dark" : ""}`}>
+			<SideBarIcon
+				icon={<GiMushroomHouse size="44" />}
+				text="Homepage"
+				handleClick={setView}
+			/>
+			<SideBarIcon
+				icon={<GiSunglasses size="44" />}
+				text="Read My CV"
+				handleClick={setView}
+			/>
+			<SideBarIcon
+				icon={<GiLaptop size="44" />}
+				text="My Projects"
+				handleClick={setView}
+			/>
+			{/* <SideBarIcon icon={<GiPalette size="44"/>} text = 'My React Components' handleClick={setView}/> */}
+			<SideBarIcon
+				icon={<GiLightBulb size="44" />}
+				text="Toggle Dark Mode"
+				handleClick={toggleDarkMode}
+			/>
+			<div className="fixed -right-3  hidden xl:block">
+				<MenuTab />
+			</div>
+		</div>
+	)
+}
 
 const SideBarIcon = ({ icon, text, handleClick }) => (
-    <div className="sidebar-icon group" onClick={() => handleClick(text)}>
-        {icon}
-        <span className="sidebar-tooltip group-hover:scale-100">
-        {text}
-        </span>
-    </div>
-);
+	<div
+		className="sidebar-icon group"
+		onClick={() => handleClick(text)}>
+		{icon}
+		<span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+	</div>
+)
 
-export default SideBar;
+export default SideBar
