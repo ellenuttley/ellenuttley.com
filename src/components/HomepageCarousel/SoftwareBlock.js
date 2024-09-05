cc76edff5bd5b08e68cd559bbf3ba3b974a5ce7c
ellenuttley <ellenuttley@gmail.com>
2024-09-05 10:46:17 +0100

    fixed container sizing issue on software block

--- src/components/HomepageCarousel/SoftwareBlock.js
+++ src/components/HomepageCarousel/SoftwareBlock.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { FiDownload, FiMinus, FiSquare, FiX } from "react-icons/fi";
+import { FiMinus, FiSquare, FiX } from "react-icons/fi";
 
 const SoftwareContent = () => {
  return (
-    <div className='relative size-full rounded-md bg-windows bg-cover dark:bg-blend-darken dark:bg-secondary/80 bg-center p-6 xl:p-16 xl:py-12 shadow-sm shadow-secondary dark:shadow-none '>
-      <div className='size-full flex flex-col bg-gradient-to-b from-[#faf9fa]  via-lightGrey to-[#d0d0d0] dark:bg-black/70 rounded-md  shadow-black/50 shadow-xl'>
+//   <div className='relative px-0.5 size-full rounded-md bg-windows rounded-t-none bg-cover dark:bg-blend-darken dark:bg-secondary/80 bg-center p-6  pt-0 mt-0  shadow-sm shadow-secondary dark:shadow-none '>
+//   <div className='size-full flex flex-col bg-gradient-to-b from-mywhite to-lightGrey dark:bg-black/70 rounded-md rounded-t-none shadow-black/50 shadow-xl'>
+//       <div className=" p-3">
+//             {" "}
+//             Software Development is my biggest passion and strongest talent. Equipt with my CFG Degree in Software Engineering, I now have  Python, React, and React Native, alongside SQL for database management. I pride myself on my ability to adapt to new technologies and methodologies quickly. 
+//           </div>
+// </div>
+// </div>
+    <div className=' container size-full items-center justify-center rounded-md bg-windows bg-cover dark:bg-blend-darken dark:bg-secondary/80 bg-center p-6 xl:p-16 xl:py-12 shadow-sm shadow-secondary dark:shadow-none '>
+      <div className=' container m-auto flex-col bg-gradient-to-b from-mywhite to-lightGrey dark:bg-black/70 rounded-md  shadow-black/50 shadow-xl'>
       {/* <div className='size-full flex flex-col bg-lightGrey dark:bg-black/70 rounded-md outline  outline-2  outline-slate-500  dark:border-none dark:outline-black shadow-black/50 shadow-xl'> */}
                   <div className=" flex  flex-row justify-start bg-gradient-to-b from-[#1C98E3]  via-[#37B9F3] to-[#027AD4] dark:bg-[#052b48] border-t-[#1C98E3] border-b-[#027AD4]/50 border border-x-2 border-x-[#37B9F3]/50 shadow-slate-700/50 shadow-sm dark:border-slate-100/50    items-center max-h-[15%] rounded-sm rounded-t-md  dark:bg-gradient-to-b dark:from-secondary/50  dark:via-[#052b48] dark:to-secondary/50">
 
-                <div className=" w-full p-2 xl:px-3 whiteHeader xl:text-2xl ">
+                <div className="p-2 xl:px-3 whiteHeader xl:text-2xl ">
                     Software Development
                 </div>
                 <div className='inline-flex *:text-slate-100 *:mx-2 xl:text-xl '>
@@ -26,6 +34,7 @@ const SoftwareContent = () => {
 							</div>
     </div>
     </div>
+    
  );
 };
 
